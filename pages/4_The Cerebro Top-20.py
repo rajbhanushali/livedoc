c2ad753c70bb7132f633fc85af340f8601201e09
cerebrosportsdev <146361023+cerebrosportsdev@users.noreply.github.com>
2024-05-27 20:13:20 -0400

    Update 4_The Cerebro Top-20.py

--- pages/4_The Cerebro Top-20.py
+++ pages/4_The Cerebro Top-20.py
@@ -2,10 +2,10 @@ import streamlit as st
 from utils import plot_bar_chart, plot_pie_chart, render_ai_button
 from sql_queries import get_table_from_snowflake
 from streamlit_extras.app_logo import add_logo
-from static_prompts import get_overview_prompt, get_skill_leader_prompt
+from static_prompts import get_overview_prompt, get_skill_leader_prompt, get_skill_leader_prompt
 import pandas as pd
-from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
-from st_aggrid import ColumnsAutoSizeMode
+from st_aggrid import AgGrid, GridOptionsBuilder, JsCode , ColumnsAutoSizeMode
+
 
 
 st.set_page_config(
